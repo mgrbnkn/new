6a2526e264161f795d8eeeab1abf30f196557bce
mgrbnkn <31654816+mgrbnkn@users.noreply.github.com>
2018-02-04 17:33:28 +0300

    Add files via upload

---
+++ Homework7/hw7.py
@@ -0,0 +1,32 @@
+#5
+#сколько в тексте прилагательных с суффиксом -ous,
+#и какая средняя длина такого прилагательного.
+
+def tokenize(filename):
+    with open(filename, encoding='utf-8') as f:
+        text = f.read()
+    text = text.replace(',', '').replace('.', '').replace(':','').replace('?','').replace('!','').replace('"','').replace(';','').replace(')','').replace('(','')
+    text = text.lower()
+    words = text.split()
+    return words
+
+def find_ous(words):
+    dict_ous = {}
+    for word in words:
+       if word[-3:] == 'ous':
+           new_word = {word: len(word)}
+           dict_ous.update(new_word)
+    return dict_ous
+
+def main():
+    average = 0
+    number = 0
+    tokens = tokenize('Pride_and_Prejudice.txt')
+    dictionary = find_ous(tokens)
+    for value in dictionary.values():
+        number = number + 1
+        average = average + value
+    average = average / number
+    print('В тексте',number,'прилагательных с суффиксом -ous и средняя длина такого прилагательного',round(average),'букв.')
+
+main()
