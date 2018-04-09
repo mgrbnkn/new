3b5b179d323df6f03ecb3aa1a0301ad78bcb0dbd
mgrbnkn <31654816+mgrbnkn@users.noreply.github.com>
2018-04-09 18:37:28 +0300

    Add files via upload

---
+++ Homework11/hw11.py
@@ -0,0 +1,24 @@
+import re
+
+def name():
+    filename = input('Введите имя файла: ')
+    return filename
+
+def open_(filename):
+    with open(filename, encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def find(text):
+    res = re.sub('Динозавр','Кот',text)
+    res = re.sub('динозавр','кот',res)
+    return res
+
+def main():
+    filename = name()
+    text = open_(filename)
+    result = find(text)
+    with open("Коты.html", "w", encoding="utf-8") as f:
+        f.write(result)
+  
+main()
